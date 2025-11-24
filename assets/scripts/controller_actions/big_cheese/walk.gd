a36bf15cb0a19ccaa27c5926fed31391fda99dc0
superthe <103425631+souperthe@users.noreply.github.com>
2025-11-24 10:07:06 -0400

    stuff

--- assets/scripts/controller_actions/big_cheese/walk.gd
+++ assets/scripts/controller_actions/big_cheese/walk.gd
@@ -1,16 +1,18 @@
 extends ControllerAction
 
+var _stepTime:float = 0
 
 func actionEnter(_message:String="")->void:
 	
 	corePlayer.animator.playAnimation("walk")
+	_stepTime = 0
 	
 	return
 	
 func actioneExit()->void:
 	return
 
-func actionPhysics(_delta:float)->void:
+func actionPhysics(delta:float)->void:
 	
 	if Input.is_action_just_pressed("player_jump"):
 		coreState.actionTransition("fly_transition", "enter")
@@ -35,6 +37,15 @@ func actionPhysics(_delta:float)->void:
 	
 	
 	corePlayer.velocity = corePlayer.wishDir * (corePlayer.walk_speed/2)
+	
+	_stepTime += corePlayer.velocity.length() * delta
+	
+	if _stepTime > 4.5:
+		coreSound.playSound(
+			"res://assets/resources/rnd_sound/metal_step.tres",
+			0.8
+			)
+		_stepTime = 0
 	
 	
 	return
