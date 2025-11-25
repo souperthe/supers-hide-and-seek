63a00a6ca3b62c4d3c98199ca968c028dd13b26d
superthe <103425631+souperthe@users.noreply.github.com>
2025-11-25 09:05:47 -0400

    scan setup

--- assets/scripts/controller_actions/big_cheese/search.gd
+++ assets/scripts/controller_actions/big_cheese/search.gd
@@ -1,13 +1,73 @@
 extends ControllerAction
 
 var _searching:bool = false
+var _headRoot:Sprite3D
+var _tween:Tween
+
+func _createScanRing(root:Node3D) -> void:
+	util.oneShotSFX3D(
+		root,
+		"res://assets/sound/sfx/hl1/fvox/bell.wav"
+	)
+	
+	
+	if _tween:
+		_tween.kill()
+	
+	
+	_headRoot.pixel_size = 0.1
+	_headRoot.transparency = 0
+	
+	_tween = get_tree().create_tween()
+	
+	_tween.set_parallel(true)
+	
+	_tween.tween_property(
+		_headRoot,
+		"pixel_size",
+		15.0,
+		0.5
+	)
+	_tween.tween_property(
+		_headRoot,
+		"transparency",
+		1,
+		0.8
+	)
+	
+	
+	return
 
 func actionEnter(_message:String="")->void:
 
 	corePlayer.animator.playAnimation("rake", 1)
 	_searching = false
+	
+	for node in util.getDescendants(corePlayer.modelRoot):
+		if node.name == "scan":
+			_headRoot = node
+			break
+		else:
+			continue
+		continue
+	
+	await get_tree().create_timer(0.6).timeout
+	
 	coreSound.playSound(
-		"res://assets/sound/sfx/character/big_cheese/TL_step_on_rake.mp3")
+		"res://assets/sound/sfx/character/big_cheese/TL_step_on_rake.mp3"
+		)
+		
+	await get_tree().create_timer(0.5).timeout
+	
+	
+	for i in range(5):
+		_createScanRing(_headRoot)
+		await get_tree().create_timer(0.8).timeout
+		
+	corePlayer.animator.animationSetSpeed(1)
+	
+	
+	
 	
 	return
 	
