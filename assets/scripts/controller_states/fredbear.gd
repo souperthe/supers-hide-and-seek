693b995632efdfa9be82b68e1d1b9ec0ec41af30
reckdave <recksttacc@gmail.com>
2025-11-28 18:40:33 +0000

    forgot to remove this line

--- assets/scripts/controller_states/fredbear.gd
+++ assets/scripts/controller_states/fredbear.gd
@@ -4,7 +4,6 @@ var runTimer:float = 0
 
 
 func controllerStart(_message:String="") -> void:
-	corePlayer.animator.animatorSetup()
 	corePlayer.neckOffset.position.y = 1
 	actionTransition(_initalAction)
 	return
